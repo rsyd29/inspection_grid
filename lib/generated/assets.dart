8f518bd22b115db55309d31fbd65602374521565
Budiman Rasyid <budiman.rasyid@legoas.co.id>
2024-11-08 10:45:40 +0700

    Create concept for inspection with grid view thumbnail

--- lib/generated/assets.dart
+++ lib/generated/assets.dart
@@ -3,5 +3,21 @@ class Assets {
   Assets._();
 
   static const String imagesMobil = 'assets/images/mobil.png';
+  static const String imagesMobilPart00 = 'assets/images/mobil_part_0_0.png';
+  static const String imagesMobilPart01 = 'assets/images/mobil_part_0_1.png';
+  static const String imagesMobilPart02 = 'assets/images/mobil_part_0_2.png';
+  static const String imagesMobilPart03 = 'assets/images/mobil_part_0_3.png';
+  static const String imagesMobilPart10 = 'assets/images/mobil_part_1_0.png';
+  static const String imagesMobilPart11 = 'assets/images/mobil_part_1_1.png';
+  static const String imagesMobilPart12 = 'assets/images/mobil_part_1_2.png';
+  static const String imagesMobilPart13 = 'assets/images/mobil_part_1_3.png';
+  static const String imagesMobilPart20 = 'assets/images/mobil_part_2_0.png';
+  static const String imagesMobilPart21 = 'assets/images/mobil_part_2_1.png';
+  static const String imagesMobilPart22 = 'assets/images/mobil_part_2_2.png';
+  static const String imagesMobilPart23 = 'assets/images/mobil_part_2_3.png';
+  static const String imagesMobilPart30 = 'assets/images/mobil_part_3_0.png';
+  static const String imagesMobilPart31 = 'assets/images/mobil_part_3_1.png';
+  static const String imagesMobilPart32 = 'assets/images/mobil_part_3_2.png';
+  static const String imagesMobilPart33 = 'assets/images/mobil_part_3_3.png';
   static const String jsonInspection = 'assets/json/inspection.json';
 }
--- lib/pages/dynamic_inspection_with_concept_3.dart
+++ lib/pages/dynamic_inspection_with_concept_3.dart
@@ -16,6 +16,25 @@ class _DynamicInspectionWithConcept3State
     extends State<DynamicInspectionWithConcept3> {
   @override
   Widget build(BuildContext context) {
+    final imageParts = [
+      'assets/images/mobil_part_0_0.png',
+      'assets/images/mobil_part_0_1.png',
+      'assets/images/mobil_part_0_2.png',
+      'assets/images/mobil_part_0_3.png',
+      'assets/images/mobil_part_1_0.png',
+      'assets/images/mobil_part_1_1.png',
+      'assets/images/mobil_part_1_2.png',
+      'assets/images/mobil_part_1_3.png',
+      'assets/images/mobil_part_2_0.png',
+      'assets/images/mobil_part_2_1.png',
+      'assets/images/mobil_part_2_2.png',
+      'assets/images/mobil_part_2_3.png',
+      'assets/images/mobil_part_3_0.png',
+      'assets/images/mobil_part_3_1.png',
+      'assets/images/mobil_part_3_2.png',
+      'assets/images/mobil_part_3_3.png',
+    ];
+
     return Scaffold(
       appBar: AppBar(
         title: Text(
@@ -23,6 +42,56 @@ class _DynamicInspectionWithConcept3State
           textAlign: TextAlign.center,
         ),
         centerTitle: true,
+      ),
+      body: Padding(
+        padding: const EdgeInsets.all(8.0),
+        child: GridView.builder(
+          itemCount: imageParts.length,
+          gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+            crossAxisCount: 4,
+            crossAxisSpacing: 4.0,
+            mainAxisSpacing: 4.0,
+          ),
+          itemBuilder: (context, index) {
+            return Stack(
+              children: [
+                Container(
+                  decoration: BoxDecoration(
+                    image: DecorationImage(
+                      image: AssetImage(imageParts[index]),
+                      fit: BoxFit.cover,
+                    ),
+                    borderRadius: BorderRadius.circular(8.0),
+                  ),
+                ),
+                Positioned(
+                  bottom: 0,
+                  left: 0,
+                  right: 0,
+                  child: Container(
+                    decoration: BoxDecoration(
+                      color: Colors.black54,
+                      borderRadius: BorderRadius.only(
+                        bottomLeft: Radius.circular(8.0),
+                        bottomRight: Radius.circular(8.0),
+                      ),
+                    ),
+                    padding: const EdgeInsets.all(4.0),
+                    child: Text(
+                      'Part ${index + 1}',
+                      style: TextStyle(
+                        color: Colors.white,
+                        fontSize: 12,
+                        fontWeight: FontWeight.bold,
+                      ),
+                      textAlign: TextAlign.center,
+                    ),
+                  ),
+                ),
+              ],
+            );
+          },
+        ),
       ),
     );
   }
